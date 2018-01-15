16321a61d7b309a06730f37f2e412be0014c7dbc
PeiLin-UW-SE <peilin.li@yahoo.ca>
2018-01-14 19:16:33 -0500

    cleaned up server code.

--- chatServer.py
+++ chatServer.py
@@ -1,34 +1,42 @@
+#!/usr/bin/python3
+
 import socket
-
-database = []
-
-state = True
+import sys
+import signal
 
 
-def server():
-    global state
-    host = "127.0.0.1"
-    port = 5000
+class Server:
+
+    clients = []
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind((host, port))
 
-    print("----- SERVER IS ON-----")
+    def sigint_handler(self, signum, frame):
+        self.s.close()  # closes the socket
+        sys.exit(0)  # exit without error
 
-    while state:
-        encoded_data, address = s.recvfrom(1024)
-        decoded_data = encoded_data.decode("utf-8")
+    def __init__(self, host="127.0.0.1", port=5000):
+        online = True
+        self.s.bind((host, port))
+        signal.signal(signal.SIGINT, self.sigint_handler)
+        print("----- SERVER IS ON-----")
 
-        if address not in database:
-            database.append(address)
+        while online:
+            encoded_data, client = self.s.recvfrom(1024)
+            decoded_data = encoded_data.decode("utf-8")
 
-        if "SHUTDOWN" in decoded_data:
-            print("----- SERVER IS OFF -----")
-            state = False
-        else:
-            print("Received From: " + str(address) + "\n    " + str(decoded_data))
-            for client in database:
-                s.sendto(encoded_data, client)
-    s.close()
+            if client not in self.clients:
+                self.clients.append(client)
+
+            if "SHUTDOWN" in decoded_data:
+                print("----- SERVER IS OFF -----")
+                online = False
+            else:
+                print("Received From: " + str(client) + "\n    " + str(decoded_data))
+                for client in self.clients:
+                    self.s.sendto(encoded_data, client)
+        self.s.close()
+        sys.exit(0)
+
 
 if __name__ == '__main__':
-    server()
+    Server()
